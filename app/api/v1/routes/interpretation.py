d4bf1c1ece0fee85fac1d21df38fa0c6423e3f91
Ren <renan199767@gmail.com>
2025-10-19 06:32:30 +0300

    change router

--- app/api/v1/routes/interpretation.py
+++ app/api/v1/routes/interpretation.py
@@ -1,17 +1,20 @@
-from fastapi import APIRouter, Form
+from fastapi import APIRouter
 import numpy as np
 import shap
 
 router = APIRouter(prefix="/interpretation", tags=["Интерпретация предсказания для одного клиента"])
 
-@router.post("/interpretation/{id}")
-async def interpretation(
-    id: int = Form(....)
+@router.post("/interpretation/{client_id}")
+def interpretation(
+    client_id: int
 ):
-try:
-    explainer = shap.Explainer(best_model, X_train_bal)
-    expected_value = explainer.expected_value
-    shap_values = explainer(X_test)
-    
-    top10 = np.abs(shap_values[id].values).argsort()[-10:][::-1]
-    shap.force_plot(expected_value, shap_values[id].values[top10], X_train.iloc[10][top10])
+    try:
+        explainer = shap.Explainer(best_model, X_train_bal)
+        expected_value = explainer.expected_value
+        shap_values = explainer(X_test)
+
+        top10 = np.abs(shap_values[client_id].values).argsort()[-10:][::-1]
+        plot = shap.force_plot(expected_value, shap_values[client_id].values[top10], X_test.iloc[client_id][top10])
+        return {"plot": str(plot)}  
+    except Exception as e:
+        return {"error": str(e)}
--- app/api/v1/routes/training.py
+++ app/api/v1/routes/training.py
@@ -7,7 +7,7 @@ from sklearn.model_selection import train_test_split
 def train_and_log_model(X, y):
     X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2)
 
-    model = RandomForestClassifier(random_state=42)
+    model = RandomForestClassifier()
     model.fit(X_train, y_train)
 
     y_pred = model.predict(X_test)
